73dbc5104fc620b39d063bdf5015b9ca9a13ca7d
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-12-03 10:29:40 +0330

    - setup typeorm and database configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseProvider } from './providers/typeorm.provider';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [],
-  providers: [],
+  providers: [DatabaseProvider],
+  exports: [DatabaseProvider],
 })
 export class AppModule {}
