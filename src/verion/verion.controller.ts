b64741b8926560a37c929839a25bf46aa731f62b
Daniel <e@sohe.ir>
2023-12-25 14:22:56 +0330

    chore: make verion api public

--- src/verion/verion.controller.ts
+++ src/verion/verion.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import * as fs from 'fs/promises';
+import { IsPublic } from '../auth/decorators/is_public.decorator.ts';
 
 @Controller('verion')
 export class VerionController {
   @Get()
+  @IsPublic()
   async vertion() {
     const getGitId = async () => {
       const gitId = await fs.readFile('.git/HEAD', 'utf8');
