ec26ba77f326bf3e6a90dfaddb7b9a731bfc8fa1
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-12-25 13:50:19 +0330

    fix ci errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { HttpException, HttpStatus, Injectable, Req } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersPanelService } from 'src/users/services/users.panel.service';
 import * as bcrypt from 'bcrypt';
-import { Request } from 'express';
+// import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -41,7 +41,7 @@ export class AuthService {
     };
   }
 
-  async logout(@Req() request: Request): Promise<any> {
-    //@ts-ignore
-  }
+  // async logout(@Req() request: Request): Promise<any> {
+  //   //@ts-ignore
+  // }
 }
