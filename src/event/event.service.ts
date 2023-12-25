2ff160081765ae30adfb20ff2889b5cd89fb8443
Daniel <daniel@sohe.ir>
2023-12-25 20:38:20 +0330

    refactor: format with prettier

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,1 +1,45 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';import { InjectModel } from '@nestjs/sequelize';import { Event } from './entities/event.entity';import { Sequelize } from 'sequelize-typescript';import { REQUEST } from '@nestjs/core';import { Request } from 'express';import { doInTransaction } from '../transaction';import { CreateEventDTO } from './dto/create.dto';@Injectable()export class EventService {  private logger = new Logger(EventService.name);  constructor(    @InjectModel(Event)    private eventRepository: typeof Event,    private sequelize: Sequelize,    @Inject(REQUEST) private request: Request,  ) {}  async create(payload: CreateEventDTO) {    return doInTransaction(this.sequelize, async (transaction) => {      return this.eventRepository.create({ ...payload }, { transaction });    });  }  async update(uuid: string, payload: CreateEventDTO) {    return doInTransaction(this.sequelize, (transaction) => {      return this.eventRepository.update(        { ...payload },        { transaction, where: { uuid } },      );    });  }  async delete(uuid: string) {    return doInTransaction(this.sequelize, (transaction) => {      return this.eventRepository.destroy({ transaction, where: { uuid } });    });  }  async getById(uuid: string) {    return this.eventRepository.findOne({ where: { uuid } });  }  async getAll() {    return this.eventRepository.findAll();  }}
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Event } from './entities/event.entity';
+import { Sequelize } from 'sequelize-typescript';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
+import { doInTransaction } from '../transaction';
+import { CreateEventDTO } from './dto/create.dto';
+
+@Injectable()
+export class EventService {
+  private logger = new Logger(EventService.name);
+  constructor(
+    @InjectModel(Event)
+    private eventRepository: typeof Event,
+
+    private sequelize: Sequelize,
+    @Inject(REQUEST) private request: Request,
+  ) {}
+
+  async create(payload: CreateEventDTO) {
+    return doInTransaction(this.sequelize, async (transaction) => {
+      return this.eventRepository.create({ ...payload }, { transaction });
+    });
+  }
+  async update(uuid: string, payload: CreateEventDTO) {
+    return doInTransaction(this.sequelize, (transaction) => {
+      return this.eventRepository.update(
+        { ...payload },
+        { transaction, where: { uuid } },
+      );
+    });
+  }
+  async delete(uuid: string) {
+    return doInTransaction(this.sequelize, (transaction) => {
+      return this.eventRepository.destroy({ transaction, where: { uuid } });
+    });
+  }
+  async getById(uuid: string) {
+    return this.eventRepository.findOne({ where: { uuid } });
+  }
+  async getAll() {
+    return this.eventRepository.findAll();
+  }
+}
