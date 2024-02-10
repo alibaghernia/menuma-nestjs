8d1e50f5eff6ecc292b2ab7d0a33bd3b425a6231
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-02-10 11:44:22 +0330

    - update migration

--- src/database/migrations/20231231183468-change-name-of-some-fields-in-customers-table.ts
+++ src/database/migrations/20231231183468-change-name-of-some-fields-in-customers-table.ts
@@ -5,7 +5,7 @@ import { QueryInterface } from 'sequelize';
 module.exports = {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.renameColumn('customers', 'name', 'first_name');
-    await queryInterface.renameColumn('customers', 'surName', 'last_name');
+    await queryInterface.renameColumn('customers', 'sur_name', 'last_name');
     await queryInterface.renameColumn('customers', 'birthday', 'birth_date');
   },
 
