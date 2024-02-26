be4a4da0b4056c374099a42cf1d3a5b123300cc0
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-02-26 15:03:21 +0330

    - update env and code

--- src/web_push/web_push.service.ts
+++ src/web_push/web_push.service.ts
@@ -1,20 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import * as webpush from 'web-push';
 
 @Injectable()
 export class WebPushService {
   private subscribers: Record<string, webpush.PushSubscription[]> = {};
 
-  constructor(configService: ConfigService) {
-    const publicKey = configService.get('WEB_PUSH_PUBLIC_KEY');
-    const privateKey = configService.get('WEB_PUSH_PRIVATE_KEY');
-    if (!publicKey || !privateKey) {
-      console.error('Check web-push keys!');
-      process.exit(1);
-    }
-    webpush.setVapidDetails('https://menuma.online', publicKey, privateKey);
-  }
+  constructor() {}
 
   subscribe(business_uuid: string, subs: any) {
     if (!this.subscribers[business_uuid]) this.subscribers[business_uuid] = [];
