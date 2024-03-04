320549d3e2af6747caa7ee0496716a4a14c02928
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-03-04 18:41:53 +0330

    fix pagination bug

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -38,7 +38,7 @@ export const getPagination = (filters: PaginationDto) => {
   const offset = filters.page
     ? +filters.page * +filters.limit - +filters.limit
     : 0;
-  const limit = filters.limit ? offset + +filters.limit : undefined;
+  const limit = filters.limit ? +filters.limit : undefined;
   return {
     offset,
     limit,
